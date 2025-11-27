b7e4f2c2dfee159a621fe024ed8b7a75f95020da
anshmarfatiya29 <anshmarfatiya29@gmail.com>
2025-11-27 23:28:55 +0530

    12

--- frontend/expense-tracker/src/components/Dashboard/FinanceOverview.jsx
+++ frontend/expense-tracker/src/components/Dashboard/FinanceOverview.jsx
@@ -20,7 +20,7 @@ const FinanceOverview = ({totalBalance, totalIncome, totalExpense}) => {
         <CustomPieChart
             data={balanceData}
             label="Total Balance"
-            totalAmount={`$${totalBalance}`}
+            totalAmount={`₹${totalBalance}`}
             colors={COLORS}
             showTextAnchor
         />
